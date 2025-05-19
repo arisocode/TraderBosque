398857ba8f58e8df86221e93e9ee0df46033ccb5
Mathew Garzon <mjgarzonl@unbosque.edu.co>
2025-05-18 23:37:29 -0500

    merge con protafolio

--- traderbosque-frontend/src/app/components/NavBar.tsx
+++ traderbosque-frontend/src/app/components/NavBar.tsx
@@ -1,57 +1,17 @@
 import {
-    Avatar,
-    Dropdown,
-    DropdownDivider,
-    DropdownHeader,
-    DropdownItem,
     Navbar,
     NavbarBrand,
-    NavbarCollapse,
-    NavbarLink,
-    NavbarToggle,
 } from "flowbite-react";
 
-//ATENCIÓN PODRIA UTILIZARSE ICONOS EN VEZ DE AVATAR
-/*
-Este componente es un NavBar proveniente de FlowBite que esta basado en tailwind CSS
-Disponible en: https://flowbite-react.com/docs/components/navbar
-*/
 export function NavBar() {
     return (
-        <Navbar fluid rounded>
+        <Navbar fluid rounded className="bg-[#1D5245] text-[#CB8A59]">
             <NavbarBrand href="../home">
-                <img src='/logo.svg' className="mr-3 h-6 sm:h-9" alt="Acciones ElBosque" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Acciones ElBosque</span>
+                <img src='/traderbosque.png' className="mr-3 h-6 sm:h-9" alt="Acciones ElBosque" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold">
+                    TraderBosque
+                </span>
             </NavbarBrand>
-            <div className="flex md:order-2">
-                <Dropdown
-                    arrowIcon={false}
-                    inline
-                    label={
-                        <Avatar alt="User settings" img="https://static.wikia.nocookie.net/amazon-invincible/images/d/d8/ViltrumiteMarkHighRes.png/revision/latest/scale-to-width-down/1200?cb=20241216223033" rounded /> //JUSTO EN ESTA PARTE
-                    }
-                >
-                    <DropdownHeader>
-                        <span className="block text-sm">Bonnie Green</span>
-                        <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-                    </DropdownHeader>
-                    <DropdownItem>Dashboard</DropdownItem>
-                    <DropdownItem>Settings</DropdownItem>
-                    <DropdownItem>Earnings</DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem>Sign out</DropdownItem>
-                </Dropdown>
-                <NavbarToggle />
-            </div>
-            <NavbarCollapse>
-                <NavbarLink href="#" active>
-                    Home
-                </NavbarLink>
-                <NavbarLink href="#">About</NavbarLink>
-                <NavbarLink href="#">Services</NavbarLink>
-                <NavbarLink href="#">Pricing</NavbarLink>
-                <NavbarLink href="#">Contact</NavbarLink>
-            </NavbarCollapse>
         </Navbar>
     );
 }
--- traderbosque-frontend/src/app/components/Toast.tsx
+++ traderbosque-frontend/src/app/components/Toast.tsx
@@ -1,7 +1,6 @@
 import { showToast } from "nextjs-toast-notify";
 import React, { useEffect, useRef } from 'react';
 
-
 interface Prop {
     message: string;
 }
@@ -31,11 +30,11 @@ export const ErrorToast: React.FC<Prop> = ({ message }) => {
         if (!shown.current) {
             shown.current = true;
             showToast.error(message, {
-                duration: 5000,
+                duration: 4000,
                 progress: true,
                 position: "top-right",
                 transition: "fadeIn",
-                icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>',
+                icon: '',
                 sound: false,
             });
         }
@@ -51,11 +50,11 @@ export const WarningToast: React.FC<Prop> = ({ message }) => {
         if (!shown.current) {
             shown.current = true;
             showToast.warning(message, {
-                duration: 5000,
+                duration: 4000,
                 progress: true,
                 position: "top-right",
                 transition: "fadeIn",
-                icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>',
+                icon: '',
                 sound: false,
             });
         }
@@ -70,12 +69,12 @@ export const InfoToast: React.FC<Prop> = ({ message }) => {
     useEffect(() => {
         if (!shown.current) {
             shown.current = true;
-            showToast.info(message, {
-                duration: 5000,
+            showToast.warning(message, {
+                duration: 4000,
                 progress: true,
                 position: "top-right",
                 transition: "fadeIn",
-                icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>',
+                icon: '',
                 sound: false,
             });
         }
